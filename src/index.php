e52dd7367069ba4e345caa9e4d77f80c080f0030
DeCe <davidcerdan20@gmail.com>
2026-02-10 14:33:42 +0100

    Configuracion Database

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 // Tu cadena de conexión (Lo ideal es que esto venga de getenv('DATABASE_URL'))
-$databaseUrl = "postgresql://fly-user:tPZ1lHoJxuKM7vHh3KbEB9Xh@pgbouncer.z23750v7myl096d1.flympg.net/fly-db";
+$databaseUrl = "postgresql://postgres:eYmLnUcbLPPaufppwTnKQCiziBAzJeCO@mainline.proxy.rlwy.net:36521/railway";
 
 // Parseamos la URL para extraer los componentes
 $dbConfig = parse_url($databaseUrl);
